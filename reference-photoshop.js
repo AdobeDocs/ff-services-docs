0aa1eb0426e86d59a6883d55b12ed2f8ddcbcd33
timkim <timk@adobe.com>
2024-03-11 17:28:32 -0700

    Add in static and transclude sidenav

---
+++ reference-photoshop.js
@@ -0,0 +1,22 @@
+module.exports=[
+    {
+        title: 'Getting Started',
+        path: '/guides/photoshop/getting-started/'
+      },
+      {
+        title: 'General Workflow',
+        path: '/guides/photoshop/general-workflow/'
+      },
+      {
+        title: 'Features',
+        path: '/guides/photoshop/features/'
+      },
+      {
+        title: 'Code Samples',
+        path: '/guides/photoshop/code-sample/'
+      },
+      {
+        title: 'Support',
+        path: '/guides/photoshop/code-sample/'
+      },     
+  ]
