1fa288598c3595ab7521c728cf03907b49a39aee
Holly Schinsky <hschinsk@adobe.com>
2024-03-20 16:23:48 -0400

    Flatten FF and Ps/Lr API left navs

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,20 +42,11 @@ module.exports = {
         path: 'https://experienceleague.adobe.com/docs/experience-platform/intelligent-services/content-commerce-ai/overview.html'
       },
     ],
-    subPages: [      
+    subPages: [  
+      // Main Firefly Services docs - left side nav    
       {
         title: 'Overview',
         path: '/guides/',
-      },
-      {
-        title: 'Firefly API Overview',
-        path: '/firefly-api/',
-        pages: require("./reference-firefly.js"),   
-      },  
-      {
-        title: 'Photoshop API Overview',
-        path: '/photoshop/',     
-        pages: require("./reference-photoshop.js"),   
       },
       {
         title: 'Getting Started',
@@ -75,36 +66,151 @@ module.exports = {
         title: 'SDKs',
         path: '/guides/sdks'
       },
+      ///////////////////////////////////////////////
+      // Firefly API-specific left side nav      
+      ///////////////////////////////////////////////
+      {
+        title: 'Overview',
+        path: '/firefly-api/',
+      },
+      {
+        title: 'Quickstart',
+        path: '/firefly-api/guides/'
+      },
+      {
+        title: 'Concepts',
+        path: '/firefly-api/guides/concepts/',
+        pages: [
+          {
+            title: 'Get Credentials',
+            path: '/firefly-api/guides/concepts/create-credentials/'
+          },
+          {
+            title: 'Authentication',
+            path: '/firefly-api/guides/concepts/authentication/'
+          },
+          {
+            title: 'Image Model Styles',
+            path: '/firefly-api/guides/concepts/styles/'
+          },
+          {
+            title: 'Rate limits',
+            path: '/firefly-api/guides/concepts/rate-limits/'
+          },
+          {
+            title: 'Seed ID',
+            path: '/firefly-api/guides/concepts/seed-id'
+          }
+        ]
+      },
+      {
+        title: 'How-Tos',
+        path: '/firefly-api/guides/how-tos/create-your-first-ff-application.md',
+        pages: [
+          {
+            title: 'Create Your First Firefly API Implementation',
+            path: '/firefly-api/guides/how-tos/create-your-first-ff-application.md'
+          },
+          {
+            title: 'Harnassing the Firefly Services APIs in PowerAutomate',
+            path: '/firefly-api/guides/how-tos/firefly-services-with-powerautomate'
+          }
+        ]
+      },
+      {
+        title: 'API Reference',
+        path: '/firefly-api/guides/api/upload_image',
+        pages: [
+          {
+            title: "Upload Image",
+            path: "/firefly-api/guides/api/upload_image/"
+          },
+          {
+            title: "Text to Image",
+            path: "/firefly-api/guides/api/image_generation/"
+          },
+          {
+            title: "Generative Expand",
+            path: "/firefly-api/guides/api/generative_expand/"
+          },
+          {
+            title: "Generative Fill",
+            path: "/firefly-api/guides/api/generative_fill/"
+          }
+        ]
+      },
+      {
+        title: 'Release Notes',
+        path: '/firefly-api/guides/release-notes/',
+      },
+      {
+        title: 'Help',
+        path: '/firefly-api/guides/help/best-practices/',
+        pages: [
+          {
+            title: 'Best Practices',
+            path: '/firefly-api/guides/help/best-practices/'
+          },
+          {
+            title: 'Troubleshooting',
+            path: '/firefly-api/guides/help/troubleshooting/'
+          }
+        ]
+      },         
+      // END Firefly API left nav
+      ///////////////////////////////////////////////
+      // Photoshop API-specific left side nav
+      /////////////////////////////////////////////// 
+      {
+        title: 'Overview',
+        path: '/photoshop/',
+      },        
+      {
+        title: 'Getting Started',
+        path: '/photoshop/getting-started/'
+      },
+      {
+        title: 'General Workflow',
+        path: '/photoshop/general-workflow/'
+      },
+      {
+        title: 'Features',
+        path: '/photoshop/features/'
+      },
+      {
+        title: 'Code Samples',
+        path: '/photoshop/code-sample/'
+      },
+      {
+        title: 'API Documentation',
+        path: '/photoshop/api/',
+        pages: [
+          {
+            title: 'Lightroom AutoStraighten',
+            path: '/photoshop/api/lightroom_autoStraighten.md'
+          },
+        ]
+      },    
+      {
+        title: 'Support',
+        path: '/photoshop/support'
+      }
+      // END PS API left nav
+      /////////////////////////////////////////////////////
+      // TODO: Revisit including a file but one that's flattened, these roll to the API overview root
+      // menu
       // {
-      //   title: 'Overview',
-      //   path: '/support/',
-      //   header: true,
-      //   pages: [
-      //     {
-      //       title: 'Help',
-      //       path: '/support/'
-      //     },
-      //     {
-      //       title: 'FAQ',
-      //       path: '/support/FAQ/'
-      //     },
-      //     {
-      //       title: 'How to contribute',
-      //       path: '/support/contribute/'
-      //     }
-      //   ]
+      //   title: 'Firefly API Overview',
+      //   path: '/firefly-api/',
+      //   pages: require("./reference-firefly.js"),   
+      // },        
+      // 
+      // {
+      //   title: 'Photoshop API Overview',
+      //   path: '/photoshop/',     
+      //   pages: require("./reference-photoshop.js"),   
       // },
-      // {
-      //   title: 'Community',
-      //   path: '/support/community/',
-      //   header: true,
-      //   pages: [
-      //     {
-      //       title: 'Information',
-      //       path: '/support/community/'
-      //     }
-      //   ]
-      // }
+      ///////////////////////////////////////////////////// 
     ]
   },
   plugins: [`@adobe/gatsby-theme-aio`]
