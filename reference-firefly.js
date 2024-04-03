a0ac33a6c5612dd5db93a6131d668faf87a6c165
Nimitha Jalal <nimitha.sreyas@gmail.com>
2024-04-03 12:30:22 +0530

    update-reference

--- reference-firefly.js
+++ reference-firefly.js
@@ -53,21 +53,21 @@ module.exports = [
         path: '/firefly-api/guides/api/upload_image',
         pages: [
             {
-                title: "Upload Image",
+                title: "Upload",
                 path: "/firefly-api/guides/api/upload_image/"
-            },
-            {
-                title: "Text to Image",
+              },
+              {
+                title: "generateImages",
                 path: "/firefly-api/guides/api/image_generation/"
-            },
-            {
-                title: "Generative Expand",
+              },
+              {
+                title: "expandImage",
                 path: "/firefly-api/guides/api/generative_expand/"
-            },
-            {
-                title: "Generative Fill",
+              },
+              {
+                title: "fillImage",
                 path: "/firefly-api/guides/api/generative_fill/"
-            }
+              }
         ]
     },
     {
