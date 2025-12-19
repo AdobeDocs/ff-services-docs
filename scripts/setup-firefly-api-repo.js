b40bf67540e0d9ef13f4641085915ba776d2daef
Alex Abreu <aabreu@adobe.com>
2025-12-19 12:25:56 -0800

    Makes global nav "Firefly Services" -> "All Firefly Services"

--- scripts/setup-firefly-api-repo.js
+++ scripts/setup-firefly-api-repo.js
@@ -70,7 +70,7 @@ const CONFIG = {
 const NEW_SITE_METADATA = `  siteMetadata: {
     pages: [
       {
-        title: 'Firefly Services',
+        title: 'All Firefly Services',
         path: 'https://developer.adobe.com/firefly-services/docs/guides/?aio_internal'
       },
       {
